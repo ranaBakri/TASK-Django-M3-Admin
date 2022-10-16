b5a7e045f58aa58e8ee6fe163f02566d894821e1
Rana Bakri <rana.bakri@rawdalsaleheen.edu.kw>
2022-10-16 17:32:04 +0300

    rana task 3

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Pokemon
+# Register your models here.
 
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'hp', 'active')
+    list_display_links = ('id', 'name')
+    list_filter = ('active',)
+    readonly_fields = ('created_at', 'updated_at',)
+    fieldsets = (
+        (
+            "general", {
+                'fields': ('name', 'hp', 'active', 'type',)
+            }
+        ),
+        (
+            'Localizations', {
+                'fields': ('name_ar', 'name_fr', 'name_jp',),
+                'classes': ('collapse')
+
+            }
+        ),
+        (
+            None, {
+                'fields': ("created_at", "updated_at",)
+            }
+        )
+    )
 # Register your models here.
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,32 @@
+from unittest.util import _MAX_LENGTH
+from wsgiref.validate import validator
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class Pokemon(models.Model):
+
+    class PokemonType(models.TextChoices):
+        WATER = 'WA'
+        GRASS = 'GR'
+        GHOST = 'GH'
+        STEEL = 'ST'
+        FAIRY = 'FA'
+
+    name = models.CharField(max_length=30)
+    type = models.CharField(
+        max_length=2, choices=PokemonType.choices, default="WA")
+    hp = models.PositiveIntegerField(validators=[MinValueValidator(
+        50, "Can't be below 50"), MaxValueValidator(350)])
+    name_fr = models.CharField(max_length=30, default='', blank=True)
+    name_ar = models.CharField(max_length=30, default='', blank=True)
+    name_jp = models.CharField(max_length=30, default='', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True,)
+
+
+def __str__(self):
+    return self.name
